2517244e1131b53b1c32624c5b5accba6a3ede9a
Laurens Chaves Méndez <laurens004@gmail.com>
2018-02-05 22:42:08 -0600

    views updated

--- app/app.js
+++ app/app.js
@@ -13,7 +13,7 @@ var states = [
         { name: 'login', state: { url: '/login', parent: 'base', templateUrl: 'views/login.html', controller: 'LoginCtrl', data: {text: "Login", visible: false } } },
         { name: 'signup', state: { url: '/signup', parent: 'base', templateUrl: 'views/signup.html', controller: 'SignupCtrl', data: {text: "Signup", visible: false } } },
         { name: 'dashboard', state: { url: '/dashboard', parent: 'base', templateUrl: 'views/dashboard.html', controller: 'DashboardCtrl', data: {text: "Dashboard", visible: false } } },
-        { name: 'overview', state: { url: '/overview', parent: 'dashboard', templateUrl: 'views/dashboard/overview.html', data: {text: "Overview", visible: true } } },
+        { name: 'emailSearch', state: { url: '/emailSearch', parent: 'dashboard', templateUrl: 'views/dashboard/emailSearch.html', data: {text: "Email Search", visible: true } } },
         { name: 'reports', state: { url: '/reports', parent: 'dashboard', templateUrl: 'views/dashboard/reports.html', data: {text: "Reports", visible: true } } },
         { name: 'logout', state: { url: '/login', data: {text: "Logout", visible: true }} }
     ];
@@ -23,7 +23,7 @@ angular.module('BVTest', [
   'snap',
   'ngAnimate'])
   .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('/dashboard', '/dashboard/overview');
+    $urlRouterProvider.when('/dashboard', '/dashboard/emailSearch');
     $urlRouterProvider.otherwise('/login');
     angular.forEach(states, function (state) {
       $stateProvider.state(state.name, state.state);
